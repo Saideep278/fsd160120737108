f0c5e7b6849c75c08553966cfe61900f5a9a8511
Saideep278 <86518027+Saideep278@users.noreply.github.com>
2022-09-26 15:52:19 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,53 +1,74 @@
-import React, {useState,useEffect} from "react";
-import "./App.css";
-import Axios from 'axios'
-function App()
-{
-  const[sname,setStudentName]= useState("");
-  const[tech,setTechnology]= useState("");
-  const[status,setStatus]= useState("");
-  
-  const submitReview=()=>
-  {
-    Axios.post('http://localhost:9000/aliens',
-    {name:sname,
-    tech:tech,
-    sub:status}).then(()=>
-    {
-      alert("success");
-    });
-  };
-  return (
-  <div className="App">
-    <h1>CRUD Application Demo</h1>
-    <div className="information">
-    <label><b>Student Name</b></label>
-    <input 
-          type="text" 
-          name="sname" 
-          onChange={(e)=>{
-            setStudentName(e.target.value);
-          }}
-          required/>
-    <label><b>Technology</b></label>
-    <input 
-          type="text" 
-          name="tech"
-          onChange={(e)=>{
-            setTechnology(e.target.value);
-          }} 
-          required/>
-    <label><b>Status</b></label>
-    <input 
-          type="text" 
-          name="status" 
-          onChange={(e)=>{
-            setStatus(e.target.value);
-          }}
-          required/>
-    <button onClick={submitReview}><b>Submit</b></button>
-    </div>
-  </div>);
+import './App.css';
 
+function App() {
+  return (
+
+    <div class="">
+
+      <div class="" >
+        <nav class="navbar navbar-expand-lg navbar-info bg-info">
+          <a class="navbar-brand" href="#">FoodAdvisor</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Sign in </a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Log in </a>
+              </li>
+            </ul>
+          </div>
+
+        </nav>
+      </div>
+
+
+      <div class="container-fluid">
+        <h1 class="display-4">Hello, world!</h1>
+      </div>
+        
+      <div class="container-fluid"> 
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </div>
+
+      <br></br>
+      <div class="container bg-info">
+        <div class="row">
+          <div class="col-sm">
+          <br></br>
+            One of three columns
+          </div>
+          <div class="col-sm">
+          <br></br>
+            two of three columns
+          </div>
+          <div class="col-sm">
+          <br></br>
+            three of three columns
+          </div>          
+        </div>
+      </div>
+
+      <div class="container bg-info">
+        <div class="row">
+          <div class="col-sm">
+          <br></br>
+            One of 2 columns
+          </div>
+          <div class="col-sm">
+          <br></br>
+            two of 2 columns
+          </div>        
+        </div>
+      </div>
+
+      
+    </div>
+  );
 }
+
 export default App;
